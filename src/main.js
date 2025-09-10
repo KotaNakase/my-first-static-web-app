bfe4c285d711ca69364307291919f4e1f046ae69
KotaNakase <nakko1130@gmail.com>
2025-09-10 17:34:38 +0900

    データ表示

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import b-table
+import BootstrapVue from "bootstrap-vue";
 
 Vue.config.productionTip = false
+Vue.config.silent = false;
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
